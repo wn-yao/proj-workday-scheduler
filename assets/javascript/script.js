bfb8eaeb060ac17d93326598963d2cfd918e36ec
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-29 16:09:52 -0700

    changed the time block div section from using id to data attribute

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,7 @@
 var currentDayEl = $('#currentDay');
-var containerEl = $('container');
+var containerEl = $('.container');
+var availableHours = $('.time-block');
+console.log(availableHours);
 
 //display current date and time on when page load.
 
@@ -7,3 +9,6 @@ var currentDateTime = setInterval(function() {
     var rightNow = moment().format('MMM DD, YYY [at] hh:mm:ss a'); 
     currentDayEl.text(rightNow);
 },1000);
+
+// set background color for past (grey) preset (green) and future(blue)
+

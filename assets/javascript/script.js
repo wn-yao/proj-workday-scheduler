15f43dd456a2a0f0489a14d7065599a0b579cd29
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-29 14:11:21 -0700

    added time blocks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 var currentDayEl = $('#currentDay');
 var containerEl = $('container');
 
+//display current date and time on when page load.
+
 var currentDateTime = setInterval(function() {
     var rightNow = moment().format('MMM DD, YYY [at] hh:mm:ss a'); 
     currentDayEl.text(rightNow);

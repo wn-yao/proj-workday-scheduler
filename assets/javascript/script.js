c201b1c1045ea2853c1b8fce1cf84bc1b5c9738d
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-28 23:13:44 -0700

    able to display time on page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,7 @@
-var currentDayEl = $('currentDay');
+var currentDayEl = $('#currentDay');
 var containerEl = $('container');
 
-//currently not working and unsure why, able to show in console.log
 var currentDateTime = setInterval(function() {
     var rightNow = moment().format('MMM DD, YYY [at] hh:mm:ss a'); 
     currentDayEl.text(rightNow);
-    console.log(rightNow);
 },1000);

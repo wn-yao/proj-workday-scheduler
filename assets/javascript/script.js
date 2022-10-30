19f86621069734d230ddc92bde884cf6f61a43f4
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-30 12:39:52 -0700

    using different method to set the timeblock background color

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,35 +1,33 @@
 var currentDayEl = $('#currentDay');
 var containerEl = $('.container');
 var availableHours = $('.time-block');
-//console.log(availableHours);
-
+console.log(availableHours);
+//console.log(containerEl);
 //display current date and time on when page load.
 
-var currentDateTime = setInterval(function() {
-    var rightNow = moment().format('MMM DD, YYY [at] hh:mm:ss a'); 
+var currentDateTime = setInterval(function () {
+    var rightNow = moment().format('MMM DD, YYY [at] hh:mm:ss a');
     currentDayEl.text(rightNow);
-},1000);
+}, 1000);
 
-// set background color for past (grey) preset (green) and future(blue)
+// set background color 
 
 var currentHour;
+currentHour = moment().format('H');
+console.log(currentHour);
+
 var currentTimeBlcok;
 
-function setBackgroundColor () {
-    for (i = 0; i < availableHours.length; i++) {
-        currentHour = moment().format('H');
-        console.log(currentHour);
-        //getting the data attribute time for each available hours
-        currentTimeBlcok = availableHours[i].getAttirbute("data-timeBlock");
-        console.log(currentTimeBlcok);
+availableHours.each(function () {
+    currentTimeBlcok = parseInt($(this).attr("data-timeBlcok"));
+    console.log(currentTimeBlcok);
+    if (currentHour > currentTimeBlcok) {
+        $(this).find('textarea').addClass("past");
+    } else if (currentHour < currentTimeBlcok) {
+        $(this).find('textarea').addClass("future");
+    } else {
+        $(this).find('textarea').addClass("present")
     }
-    if (currentTimeBlcok < currentHour){ 
-        $(availableHours[i]).find("textarea").addClass("past");
-    }
-    if(currentHour === currentTimeBlcok) {
-        $(availableHours[i]).find("textarea").addClass("present");
-    }
-    if (currentTimeBlcok > currentHour) {
-        $(availableHours[i]).find("textarea").addClass("future");
-    }
-}
+})
+
+;

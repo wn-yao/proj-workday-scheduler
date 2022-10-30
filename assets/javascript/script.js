7871f61446cd5e1ca1e4acd2aa9ef352acbc7417
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-30 10:52:00 -0700

    trying to set background color but not working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 var currentDayEl = $('#currentDay');
 var containerEl = $('.container');
 var availableHours = $('.time-block');
-console.log(availableHours);
+//console.log(availableHours);
 
 //display current date and time on when page load.
 
@@ -12,3 +12,24 @@ var currentDateTime = setInterval(function() {
 
 // set background color for past (grey) preset (green) and future(blue)
 
+var currentHour;
+var currentTimeBlcok;
+
+function setBackgroundColor () {
+    for (i = 0; i < availableHours.length; i++) {
+        currentHour = moment().format('H');
+        console.log(currentHour);
+        //getting the data attribute time for each available hours
+        currentTimeBlcok = availableHours[i].getAttirbute("data-timeBlock");
+        console.log(currentTimeBlcok);
+    }
+    if (currentTimeBlcok < currentHour){ 
+        $(availableHours[i]).find("textarea").addClass("past");
+    }
+    if(currentHour === currentTimeBlcok) {
+        $(availableHours[i]).find("textarea").addClass("present");
+    }
+    if (currentTimeBlcok > currentHour) {
+        $(availableHours[i]).find("textarea").addClass("future");
+    }
+}
